474dec657b9f69894a3c537848ab3118b91e9bd6
Adam Nawrocki <adam.nawroc@gmail.com>
2016-07-15 12:27:07 -0400

    basic submitting

--- js/script.js
+++ js/script.js
@@ -17,12 +17,21 @@ $(document).ready(function(){
    $("#userGuess").val("");
    var random_number = Math.floor((Math.random() * 10) + 1);
 
-
+   $("form").submit(function() {
+   var count = 0;
+   if ($("#userGuess").val() !== random_number) {
+     count += 1;
+    $("#count").text(count);
    }
+
+   })
+
+
+   }();
 
    // while (random_number !==  ) {
    // }
-   newGame();
+   // newGame();
 
    $(".new").click(function() {
     newGame();
