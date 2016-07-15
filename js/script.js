1c38bccfe1642bd12b2571bb3b98822ae3c9707a
Adam Nawrocki <adam.nawroc@gmail.com>
2016-07-15 13:10:30 -0400

    use variable

--- js/script.js
+++ js/script.js
@@ -18,13 +18,17 @@ $(document).ready(function(){
    $("#guessList").empty();
    var random_number = Math.floor((Math.random() * 10) + 1);
 
+   random_number = 5;
+
    var count = 0
    $("form").submit(function(e) {
      e.preventDefault();
-     if (parseInt($("#userGuess").val()) !== random_number) {
+    var user_guess = $("#userGuess").val();
+
+     if (parseInt(user_guess) !== random_number) {
        count += 1;
        $("#count").text(count)
-       $("#guessList").append("<li>" + $("#userGuess").val() + "</li>")
+       $("#guessList").append("<li>" + user_guess + "</li>")
      }
 
    })
